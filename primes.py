eaf3e2d4ed8bd001bb63a7f0c1f306bde740ec9f
lazyN <62480903+NDimish@users.noreply.github.com>
2022-12-09 07:42:05 +0000

    update primes.py

--- primes.py
+++ primes.py
@@ -3,4 +3,10 @@
 
 def primes(number_of_primes):
     list = []
+    for i in range(2, umber_of_primes + 1):
+        for j in range(2, int(i ** 0.5) + 1):
+            if i%j == 0:
+                break
+        else:
+            list.append(i) 
     return list
