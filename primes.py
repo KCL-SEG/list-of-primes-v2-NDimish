3893dbc08b48767c433cd8c96087aacccfa4fce7
lazyN <62480903+NDimish@users.noreply.github.com>
2022-12-09 08:09:09 +0000

    updeated primes.py 2

--- primes.py
+++ primes.py
@@ -1,12 +1,33 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
+def isPrime(n):
+    
+  if(n==1 or n==0):
+    return False
+
+  for i in range(2,n):
+
+    if(n%i==0):
+      return False
+    
+  return True
+
+
+
+
 def primes(number_of_primes):
+    if(number_of_primes < 1):
+        raise ValueError('cant work with 0 or below')
     list = []
-    for i in range(2, umber_of_primes + 1):
-        for j in range(2, int(i ** 0.5) + 1):
-            if i%j == 0:
-                break
-        else:
-            list.append(i) 
+    counter = 0
+
+
+    while(len(list) < number_of_primes):
+        if(isPrime(counter)):
+            list.append(counter)
+                        
+        counter= counter+1
     return list
+
